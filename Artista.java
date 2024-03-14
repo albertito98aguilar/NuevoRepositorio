8a413a765d183768f2ec085531c3fb62bf479bc3
albertito98aguilar <alberto.naranjo.moya.alu@iesfernandoaguilar.es>
2024-03-14 16:46:45 +0100

    NuevoNombre

--- Artista.java
+++ Artista.java
@@ -2,11 +2,23 @@ package evf.javadoc.original;
 
 public class Artista {
     String nombreArtista;
+    String generoMusica;
+
+    
 
     public Artista(String nombreArtista) {
         this.nombreArtista = nombreArtista;
     }
 
+    //Alberto Naranjo Moya
+    public void aprobar(){
+        System.out.println("Voy a aprobar");
+    }
+
+    //Este metodo cambia el genero de musica del artista.
+    public void cambioGenero(){
+        generoMusica = "Rock";
+    }
     @Override
     public String toString() {
         return "Artista{" +
